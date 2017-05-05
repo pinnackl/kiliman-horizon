5bc57e051e11c8b41c0742daad07f9bc1cec8609
Hervé Tutuaku <herve.tutuaku@gmail.com>
2017-05-05 08:53:49 +0200

    [fix] move boot loading

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ const app = express();
 require('./settings')(app);              console.log('[Kiliman]' . magenta, 'loading settings...' . cyan);
 require('./models')(app);                console.log('[Kiliman]' . magenta, 'loading models...' . cyan);
 require('./middlewares')(app);           console.log('[Kiliman]' . magenta, 'loading middlewares...' . cyan);
+require('./boot')(app);                  console.log('[Kiliman]' . magenta, 'loading initial boot scripts...' . cyan);
 require('./actions')(app);               console.log('[Kiliman]' . magenta, 'loading actions..' . cyan);
 require('./routes')(app);                console.log('[Kiliman]' . magenta, 'loading routes...' . cyan);
 require('./helpers')(app);               console.log('[Kiliman]' . magenta, 'loading helpers...' . cyan);
-require('./boot')(app);                  console.log('[Kiliman]' . magenta, 'loading initial boot scripts...' . cyan);
 
 console.log('[Kiliman]' . magenta, 'Listening on port' . cyan, `${app.hzConfig.port}...` . red);
 const httpd = app.listen(app.hzConfig.port);
