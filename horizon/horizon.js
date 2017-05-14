71b5cb779d8d8cf7dd9b824fd550e68d84a3cb80
Antoine Humbert <antoine@skale-5.com>
2017-05-14 14:31:54 +0200

    change rdbconfig

--- horizon/horizon.js
+++ horizon/horizon.js
@@ -6,8 +6,6 @@ module.exports = (app, httpd) => {
         project_name: app.hzConfig.project_name,
         rdb_host: app.hzConfig.rdb_host,
         rdb_port: app.hzConfig.rdb_port,
-        rdb_user: app.hzConfig.rdb_user,
-        rdb_password: app.hzConfig.rdb_password,
         permissions: false,
         auto_create_index: true,
         auto_create_collection: true,
