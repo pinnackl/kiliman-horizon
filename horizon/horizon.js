0298cc3af7ec36dd930a5d3462c87a92ab15fc1a
Antoine Humbert <antoine@skale-5.com>
2017-05-14 14:04:23 +0200

    add rbdconfig to horizon.js

--- horizon/horizon.js
+++ horizon/horizon.js
@@ -4,6 +4,10 @@ module.exports = (app, httpd) => {
 
     const options = {
         project_name: app.hzConfig.project_name,
+        rdb_host: app.hzConfig.rdb_host,
+        rdb_port: app.hzConfig.rdb_port,
+        rdb_user: app.hzConfig.rdb_user,
+        rdb_password: app.hzConfig.rdb_password,
         permissions: false,
         auto_create_index: true,
         auto_create_collection: true,
