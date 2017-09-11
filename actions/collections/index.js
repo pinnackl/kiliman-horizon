968b55688c2019e675a54f877d56f91525d02499
Syu93 <herve.tutuaku@gmail.com>
2017-09-12 01:58:03 +0200

    [feature] Hard coded private collection filter

--- actions/collections/index.js
+++ actions/collections/index.js
@@ -7,11 +7,19 @@ module.exports = (app) => {
     function list (req, res, next) {
         return app.horizon.adapter.getDatabaseCollections()
         .then(collections => {
+            let array = [];
+            collections.forEach(collection => {
+                // FIXME : Hard coded private collection filter
+                if (collection != "hz_collections" && collection != "hz_groups" && collection != "hz_users_auth" && collection != "users") {
+                    array.push(collection);
+                }
+            });
+            return array;
+        }).then(collections => {
             res.json({
                 database: app.hzConfig.project_name,
                 collections: collections
             });
-        })
-        .catch(res.error);
+        }).catch(res.error);
     }
 };
